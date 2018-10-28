3e8b21d565b280f7bedc07460e676d398018c68b
Jackkidan <eddie59@mail.ru>
2018-10-28 18:53:31 +0300

    commit3

--- src/test/java/FirstAutotest.java
+++ src/test/java/FirstAutotest.java
@@ -1,30 +1,17 @@
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.interactions.Mouse;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-import static java.lang.Enum.valueOf;
-import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
-
-
 public class FirstAutotest {
-
 
     WebDriver driver;
 
@@ -39,20 +26,21 @@ public class FirstAutotest {
 
     @After
     public void closeDriver() {
-        // driver.quit();
+        driver.close();
     }
 
     @Test
     public void testRgsInsurance() {
+
         //Step 2
         WebElement navBarButton = findByXpath("//*[@id='main-navbar-collapse']//a[contains(text(), 'Страхование')]");
         navBarButton.click();
+
         //Step 3
         WebElement insuranceNavBarButton = findByXpath("//*[@id='main-navbar-collapse']//a[contains(text(), 'Выезжающим за рубеж')]");
         insuranceNavBarButton.click();
 
         //Step 4
-        //
         WebElement openCalculateOnlineWindow = findByXpath("/html//div[@id='main-content']/div[@class='container container-rgs-content']/" +
                 "div[@class='row-rgs-content-two-cols']//div[@class='content-document']/div[4]/div[1]/div/" +
                 "div[@class='thumbnail-footer']/a[@href='https://www.rgs.ru/products/private_person/tour/strahovanie_turistov/calc/index.wbp']");
@@ -61,27 +49,24 @@ public class FirstAutotest {
 
 
         //Step 5
-        //driver.manage().timeouts().pageLoadTimeout(5, SECONDS);
         WebElement calculateHeader = findByXpath("//div[@class='container container-rgs-app-title']");
         String expectedText = "Калькулятор страхования путешественников онлайн";
         Assert.assertEquals("Сравнение: ", expectedText, calculateHeader.getText());
 
         //Step 6
-        //xpath не смог написать руками, пришлось юзать плагинчик.
         WebDriverWait wait = new WebDriverWait(driver, 5);
-        // Чекбокс я согласен
 
+        // Чекбокс я согласен
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//label[@class = 'adaptive-checkbox-label' and contains(text(), ' Я согласен на обработку моих персональных данных в целях расчета страховой премии. ')]")));
         WebElement checkBoxElement = driver.findElement(By.xpath("//label[@class = 'adaptive-checkbox-label' and contains(text(), ' Я согласен на обработку моих персональных данных в целях расчета страховой премии. ')]"));
         checkBoxElement.click();
+
         // Несколько в течение года
         WebElement severalTripsButton = findByXpath("//div[@id='calc-vzr-steps']/myrgs-steps-partner-auth//" +
                 "div[@class='steps']/div[1]/div[@class='step-body']//form/div[1]/btn-radio-group/div/button[2]");
         severalTripsButton.click();
 
         //Step 7
-        driver.manage().timeouts().pageLoadTimeout(5, SECONDS);
-
         WebElement inputCountries = findByXpath("//input[@class = 'form-control-multiple-autocomplete-actual-input tt-input']");
         inputCountries.sendKeys("Шенген");
         inputCountries.sendKeys(Keys.DOWN);
@@ -140,22 +125,18 @@ public class FirstAutotest {
         WebElement nameText = findByCss("[data-bind='text\\: LastName\\(\\) \\+ \\' \\' \\+ FirstName\\(\\)']");
         WebElement leisure = findByXpath("//div[@class='summary']/div[7]/div[1]/div[@class='summary-row']//small[.='(включая активный отдых)']");
 
-        Assert.assertEquals("Многократные поездки в течение года", expectedMultiTripsText, multiTrips.getAttribute("textContent").trim());
-        Assert.assertEquals("(количество дней суммарно:", expecterInsuranceText, tripInsurance.getAttribute("textContent").trim());
-        Assert.assertEquals("Шенген", expectedCountryText, tripCounry.getAttribute("textContent").trim());
-        Assert.assertEquals("12.12.1985", expectedDateOfBirthText, dateOfBirth.getAttribute("textContent").trim());
-        Assert.assertEquals("IVAN IVANOV", expectedNameText, nameText.getAttribute("textContent").trim());
-        Assert.assertEquals("Включен", expectedLeisureText, leisure.getAttribute("textContent").trim());
-
-
-
+        Assert.assertEquals("Значение не соответствует ожидаемому ", expectedMultiTripsText, multiTrips.getAttribute("textContent").trim());
+        Assert.assertEquals("Значение не соответствует ожидаемому ", expecterInsuranceText, tripInsurance.getAttribute("textContent").trim());
+        Assert.assertEquals("Значение не соответствует ожидаемому ", expectedCountryText, tripCounry.getAttribute("textContent").trim());
+        Assert.assertEquals("Значение не соответствует ожидаемому ", expectedDateOfBirthText, dateOfBirth.getAttribute("textContent").trim());
+        Assert.assertEquals("Значение не соответствует ожидаемому ", expectedNameText, nameText.getAttribute("textContent").trim());
+        Assert.assertEquals("Значение не соответствует ожидаемому ", expectedLeisureText, leisure.getAttribute("textContent").trim());
     }
-
-
 
     private WebElement findByXpath(String xpath) {
         return driver.findElement(By.xpath(xpath));
     }
+
     private WebElement findByCss(String css) {
         return driver.findElement(By.cssSelector(css));
     }
